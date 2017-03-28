30710140933e92aea6c5a16293cc59d6397d3c60
Andyotarola <andyotarola8@gmail.com>
2017-03-27 21:34:54 -0500

    blucle for

---
+++ bucles/bucle_for.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>bucle for</title>
+</head>
+
+<body>
+<?php 
+
+
+
+for ($i=0; $i <15; $i+=1) { 
+	echo "a" ."<br>";
+
+	if ($i==7) {
+		echo "interrupcion";
+		#break; #para romper el bucle
+		#continue  #sirver para saltarse una vuelta dwe blucle
+	}
+}
+
+
+
+ ?>
+</body>
+</html>
